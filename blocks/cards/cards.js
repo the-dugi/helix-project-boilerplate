8735dccc6e623215d4efde2234e5a57312c341a7
Dejan Novak <dejan.novak@prime-force.com>
2023-06-09 12:55:54 +0200

    create quote

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -13,6 +13,7 @@ export default function decorate(block) {
     ul.append(li);
   });
   ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
-  block.textContent = '';
+  block.textContent = '!!! CARDS !!!';
   block.append(ul);
+
 }
